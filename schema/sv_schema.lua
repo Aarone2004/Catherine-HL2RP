0a7018080831fc9bd763ae408fb69f1c59bfaaac
L7D <smhjyh2009@gmail.com>
2015-06-26 22:44:48 +0900

    Fixed resource bug.

--- schema/sv_schema.lua
+++ schema/sv_schema.lua
@@ -22,7 +22,7 @@ resource.AddWorkshop( "105042805" )
 
 catherine.util.AddResourceInFolder( "materials/CAT_HL2RP" )
 catherine.util.AddResourceInFolder( "materials/models/tnb" )
-catherine.util.AddResourceInFolder( "models/tnb" )
+catherine.util.AddResourceInFolder( "models/humans/tnb" )
 
 CAT_SCHEMA_COMBINEOVERLAY_LOCAL = 1
 CAT_SCHEMA_COMBINEOVERLAY_GLOBAL = 2
